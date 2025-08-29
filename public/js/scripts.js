aaf394fdc3b16f150aae63d80b76de4e5d295cd5
nildoria <sabbir.ahmed87@gmail.com>
2025-08-29 14:50:08 +0600

    Pricing from main

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,2 +1,22 @@
+// Pricing Table Toggle Buttons
+const monthlyBtn = document.getElementById("monthlyBtn");
+const yearlyBtn = document.getElementById("yearlyBtn");
+const prices = document.querySelectorAll("[data-monthly]");
+
+monthlyBtn.addEventListener("click", () => {
+  monthlyBtn.classList.add("bg-green-500", "text-white");
+  yearlyBtn.classList.remove("bg-green-500", "text-white");
+  yearlyBtn.classList.add("bg-green-500");
+
+  prices.forEach((p) => (p.textContent = p.dataset.monthly));
+});
+
+yearlyBtn.addEventListener("click", () => {
+  yearlyBtn.classList.add("bg-green-500", "text-white");
+  monthlyBtn.classList.remove("bg-green-500", "text-white");
+  monthlyBtn.classList.add("text-black");
+  prices.forEach((p) => (p.textContent = p.dataset.yearly));
+});
+
 // Year at the bottom
-document.getElementById('year').innerHTML = new Date().getFullYear();
+document.getElementById("year").innerHTML = new Date().getFullYear();
