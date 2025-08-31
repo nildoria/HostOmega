2e3cfafed8d352a772561695ea5a83394f124d6e
nildoria <sabbir.ahmed87@gmail.com>
2025-08-31 18:28:48 +0600

    Footer fix and banner meteor add

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -28,6 +28,18 @@
   }
 
   container.appendChild(frag);
+
+  // Meteors
+  var reset = function (e) {
+    e.target.className = "";
+    setTimeout(function () {
+      e.target.className = "meteor";
+    }, 0);
+  };
+  var meteors = document.querySelectorAll(".meteor");
+  for (var i = 0; i < meteors.length; i++) {
+    meteors[i].addEventListener("animationend", reset);
+  }
 })();
 
 // Pricing Toggle
